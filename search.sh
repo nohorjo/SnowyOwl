3f6838d125f82ed7fde95c84b9243be76eb76247
nohorjo <nohorjo@gmail.com>
2017-05-29 18:40:51 +0100

    Added image rotation

--- search.sh
+++ search.sh
@@ -17,14 +17,38 @@ do
     do
 	    continue
     done
+    # remove spaces from filename
+    mv ${f} $(echo ${f} | sed 's/ /_/g') 2> /dev/null
     (./frames.py ${f} && mv ${f} done/vid) &
 done
 
 # Wait for the frames extraction to finish
 while [ $(jobs -r | wc -l) -gt 1 ]
 do
-	sleep 1
+	continue
 done
+echo Extracted frames
+
+# rotate frames
+for f in $(find compare -type f | grep -ve '\.md$')
+do
+    # prevent too many processes
+    while [ $(jobs -r | wc -l) -gt $(grep -c ^processor /proc/cpuinfo) ]
+    do
+	    continue
+    done
+    for r in 90 270
+    do
+        convert ${f} -rotate ${r} ${f%.*}_${r}.${f##*.} &
+    done
+done
+
+# wait for rotation to finish
+while [ $(jobs -r | wc -l) -gt 1 ]
+do
+	continue
+done
+echo Rotated frames
 
 # compare the frames
 for f in $(find compare -type f | grep -ve '\.md$')
@@ -34,5 +58,16 @@ do
     do
 	    continue
     done
+    echo Checking ${f}
     (./classifier.py infer feature/classifier.pkl ${f} >> result.log 2> err.log ; mv ${f} done/compare) &
 done
+
+# wait for comparisons to finish
+while [ $(jobs -r | wc -l) -gt 1 ]
+do
+	continue
+done
+echo Done
+
+# kill tail
+kill $(jobs -p) > /dev/null 2> /dev/null
